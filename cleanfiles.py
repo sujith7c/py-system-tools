750166a41fdf8fc2ea9654be8adc65e9623e9f0a
Sujith Suresan <sujith7c@gmail.com>
2016-08-11 14:46:16 +0400

    Debugging

--- cleanfiles.py
+++ cleanfiles.py
@@ -4,24 +4,32 @@ now = time.time()
 #Get the file/path as argument
 
 def options(cfg):
+  cfg = []
   try:
-    opts,args = getopt.getopt(sys.argv[1:],"hp:d", ["help","path","date"])
-  except getopt.GetoptError:
+    opts,args = getopt.getopt(sys.argv[1:],"?qhpd:", ["help","path","date"])
+    print opts
+  except getopt.GetoptError, err:
     usage()
+    print str(err)
+    sys.exit(2)
   for opt,arg in opts:
     if opt in ("-h","--help"):
       usage()
     elif opt in ("-p","--path"):
-      cfg['path'] = arg
+      cfg.append(arg)
     elif opt in ("-d","--days"):
-      cfg['days'] = arg
+      cfg.append(arg)
+    else:
+      assert False, "Un handled option"
 
 
 def usage():
   print "deloldfile --help -p=PATH -d= number of days"
 
 if __name__ == "__main__":
-  options(sys.argv[1:])
+  options(sys.argv)
+  ##print sys.argv[1:]
+
 
 
 
