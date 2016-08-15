6f8c1fc3d1ca2090811cfc45b0f2d40c283008db
Sujith Suresan <sujith7c@gmail.com>
2016-08-15 10:55:50 +0400

    Added list file func

--- cleanfiles.py
+++ cleanfiles.py
@@ -8,7 +8,8 @@ def main(cfg):
   cfg['path']=""
   cfg['numdays']="" 
   try:
-    opts,args = getopt.getopt(sys.argv[1:],"?q:h:p:d:e:", ["help","path","date","expression"])
+    opts,args = getopt.getopt(sys.argv[1:],"?q:h:p:d:n:", ["quite","help","path","date","name"])
+    print opts
   except getopt.GetoptError, err:
     usage()
     print str(err)
@@ -21,20 +22,23 @@ def main(cfg):
       cfg['path'] = arg
     elif opt in ("-d","--days"):
       cfg['numdays'] = arg
-    elif opt in ("-e","--regx"):
+    elif opt in ("-n","--name"):
       cfg['exp'] = arg
     else:
       assert False, "Un handled option"
 ##------------------Write Logic for file Deletion 
   #check the file/folde exist
   if len(cfg['path']) !=0:
-      #code to check folder and files
-
-
+    #code to check folder and files
+    print cfg['path']
+    PATH =  cfg['path']
 
 def usage():
   print "cleanfiles --help -p PATH -d number of days"
 
+def list_files(path):
+  files = [file for file in os.listdir(PATH) if os.path.isfile(os.path.join(PATH,file))]    
+  
 if __name__ == "__main__":
   main(sys.argv)
   
