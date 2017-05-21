06c8f463651ed3959b19cd09caf62123aef92a75
Sujith Suresan <sujith7c@gmail.com>
2017-05-21 16:18:19 +0400

    Conditional changes

--- en_mod_rw.py
+++ en_mod_rw.py
@@ -1,19 +1,29 @@
 #!/usr/bin/python
-import os,sys
+import os,sys,re
 #Check the OS Version
 RELEASE_FILE = "/etc/redhat-release"
+RWM_FILE = "/etc/httpd/conf.modules.d/00-base.conf"
 if os.path.isfile(RELEASE_FILE):
   f=open(RELEASE_FILE,"r")
-  fl_str = f.read()
-  rel_version = fl_str.split()
-  print rel_version
+  rel_list = f.read().split()
+  if rel_list[2] == "release" and tuple(rel_list[3].split(".")) < ('8','5'):
+    print("so far good")
 else:
   raise("Unable to find the OS version")
 
+#Check Apache installed
+#TODO 
+#
 
-print sys.version_info
-if sys.version_info < (2,7):
-  print "This programm works only with the Python 2.7"
+
+#Test if the rewrite module file present
+if os.path.isfile(RWM_FILE):
+ print("re write")
+
+
+##print sys.version_info
+##if sys.version_info < (2,7):
+##  print "This programm works only with the Python 2.7"###
 
 
 
