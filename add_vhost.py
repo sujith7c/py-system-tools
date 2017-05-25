f062aebf2cc85c17a8d4a14ac5d906a983b5b298
Sujith Suresan <sujith7c@gmail.com>
2017-05-25 12:56:38 +0400

    Apache add vhost script

---
+++ add_vhost.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+#This script crate Virtual Host for Apache2
+import os,sys
+#Document Directory
+doc_root = "/var/www/html/"
+apache_conf_dir = "/etc/apache2/"
+if os.getuid() != 0:
+  print("You need to run this script with root privileges, exiting!")
+  sys.exit()
+else:
+ site = raw_input("Enter the Virtual Host name : ").strip()
+ print site
+ '''TODO:check if this site/server name exis
+ iterate through each file and read the ServerName value'''
+ 
+ split_str = re.split(r'-|\*|_|\.',site) 
+ str_escaped = re.escape(site)
+ '''TODO: List the current Virtual Hosts'''
+ 
+ 
+ '''if not exister create,open file object and create file'''
+
+
+ 
+  
+
